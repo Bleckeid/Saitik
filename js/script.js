f7a6b6018b98a10d10e6869c7d99b56b3709cae9
Bleckeid <vlad802015@gmail.com>
2025-02-05 16:41:57 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,4 +30,53 @@ function showSlides(n) {
 
 setInterval(() => plusSlides(1), 10000);
 
+
+
+var slideIndexanime = 1;
+showSlidesanime(slideIndexanime);
+
+// Next/previous controls
+function plusSlidesanime(n) {
+  showSlidesanime(slideIndexanime += n);
+}
+
+// Thumbnail image controls
+function currentSlideanime(n) {
+  showSlidesanime(slideIndexanime = n);
+}
+
+function showSlidesanime(n) {
+  var i;
+  var slidesanime = document.getElementsByClassName("mySlides-anime");
+  var dotsanime = document.getElementsByClassName("dot-anime");
+  if (n > slidesanime.length) {slideIndexanime = 1}
+  if (n < 1) {slideIndexanime = slidesanime.length}
+  for (i = 0; i < slidesanime.length; i++) {
+      slidesanime[i].style.display = "none";
+  }
+  for (i = 0; i < dotsanime.length; i++) {
+      dotsanime[i].className = dotsanime[i].className.replace(" active-anime", "");
+  }
+  slidesanime[slideIndexanime -1].style.display = "block";
+  setTimeout(showSlidesanime, 10000)
+  dotsanime[slideIndexanime -1].className += " active-anime";
+}
+
+setInterval(() => plusSlidesanime(1), 15000);
+
+const popupOverlay = document.getElementById("popup-overlay");
+const popup = document.getElementById("popup");
+ 
+function showPopup() {
+  popupOverlay.style.display = "block";
+}
+ 
+function hidePopup() {
+  popupOverlay.style.display = "none";
+}
+ 
+/* popupOverlay.addEventListener("click", hidePopup);
+popup.addEventListener("click", (event) => { event.stopPropagation(); }); */
+
+
 /* ну и зачем я это делал бл а ????? */
