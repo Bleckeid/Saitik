a13505a205f59c95ad93f9763b26fc735efd62c3
Bleckeid <vlad802015@gmail.com>
2025-02-05 22:14:34 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,24 +1,82 @@
-/* Когда пользователь нажимает на кнопку,
-переключение между скрытием и отображением раскрывающегося содержимого */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-// Закройте выпадающее меню, если пользователь щелкает за его пределами
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex -1].style.display = "block";
+  setTimeout(showSlides, 10000)
+  dots[slideIndex -1].className += " active";
 }
 
+setInterval(() => plusSlides(1), 10000);
 
+
+
+var slideIndexanime = 1;
+showSlidesanime(slideIndexanime);
+
+// Next/previous controls
+function plusSlidesanime(n) {
+  showSlidesanime(slideIndexanime += n);
+}
+
+// Thumbnail image controls
+function currentSlideanime(n) {
+  showSlidesanime(slideIndexanime = n);
+}
+
+function showSlidesanime(n) {
+  var i;
+  var slidesanime = document.getElementsByClassName("mySlides-anime");
+  var dotsanime = document.getElementsByClassName("dot-anime");
+  if (n > slidesanime.length) {slideIndexanime = 1}
+  if (n < 1) {slideIndexanime = slidesanime.length}
+  for (i = 0; i < slidesanime.length; i++) {
+      slidesanime[i].style.display = "none";
+  }
+  for (i = 0; i < dotsanime.length; i++) {
+      dotsanime[i].className = dotsanime[i].className.replace(" active-anime", "");
+  }
+  slidesanime[slideIndexanime -1].style.display = "block";
+  setTimeout(showSlidesanime, 10000)
+  dotsanime[slideIndexanime -1].className += " active-anime";
+}
+
+setInterval(() => plusSlidesanime(1), 15000);
+
+const popupOverlay = document.getElementById("popup-overlay");
+const popup = document.getElementById("popup");
+ 
+function showPopup() {
+  popupOverlay.style.display = "block";
+}
+ 
+function hidePopup() {
+  popupOverlay.style.display = "none";
+}
+ 
+/* popupOverlay.addEventListener("click", hidePopup);
+popup.addEventListener("click", (event) => { event.stopPropagation(); }); */
 
 
 /* ну и зачем я это делал бл а ????? */
